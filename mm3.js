91dd7fa7ffcd6d9e8601d76d2e03f1c062e91506
tunepad980 <139092625+tunepad980@users.noreply.github.com>
2023-07-10 08:48:46 +0530

    M1

---
+++ mm3.js
@@ -0,0 +1,2 @@
+function searchChannel(){var input,filter,ul,li,a,i,txtValue;input=document.getElementById('myInput');filter=input.value.replace(" ","").toUpperCase();ul=$('.list-group')[0];li=ul.getElementsByTagName('li');for(i=0;i<li.length;i++){a=li[i].getElementsByTagName("a")[0];txtValue=a.textContent||a.innerText;if(txtValue.replace(" ","").toUpperCase().indexOf(filter)>-1){li[i].style.display="";}else{li[i].style.display="none";}}}
+
